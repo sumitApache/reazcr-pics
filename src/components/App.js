3d74b30cdb28529ae9231049256ab342f4f472ca
sumitApache <sumit.apache@gmail.com>
2019-07-26 12:57:39 +0530

    Third-commit After Data Coming at App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px' }}>
-            <h1>Search Pics</h1>
-            <SearchBar />
-        </div>
-    );
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);        
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onFormSubmit (value) {
+        console.log(value);
+        //get unplash images from api
+    }
+
+
+    render () {
+        return (
+            <div className="ui container" style={{marginTop: '10px' }}>
+                <SearchBar onSubmit={this.onFormSubmit} />
+            </div>
+        );
+    } 
 }
+
+
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,28 @@ import React from 'react';
 
 class SearchBar extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {searchText: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({searchText: event.target.value});
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state.searchText);
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Search</label>
-                        <input type="text" name="first-name" />
+                        <input type="text" value={this.state.searchText} name="first-name" onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
